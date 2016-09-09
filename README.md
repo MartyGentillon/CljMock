440cb51655c6dedd100eb7c6fc0a5d851db31733
Marty Gentillon <marty.gentillon@gmail.com>
2016-09-08 20:19:11 -0700

    initial add of project.clj and README.md

---
+++ README.md
@@ -0,0 +1,13 @@
+# clj-mock
+
+A Clojure library for succinctly mocking Clojure functions.
+
+## Usage
+
+See /test/clj_mock/core_test.clj for usage details.
+
+## License
+
+Copyright © 2016 Marty Gentillon
+
+Distributed under The MIT License (MIT)
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject com.gentillon-computing/clj-mock "0.1.0-SNAPSHOT"
+  :description "A succinct clojure mocking library"
+  :url "https://github.com/MartyGentillon/clj-mock"
+  :license {:name "MIT License"
+            :url "http://www.opensource.org/licenses/mit-license.php"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
